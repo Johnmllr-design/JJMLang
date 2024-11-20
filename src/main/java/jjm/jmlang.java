0e92961803d3c6751a460a94c7a09f01514bae08
John Miller <millerjo2@grinnell.edu>
2024-11-20 13:42:11 -0600

    initial commit

---
+++ src/main/java/jjm/jmlang.java
@@ -0,0 +1,19 @@
+package jjm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class jmlang {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
